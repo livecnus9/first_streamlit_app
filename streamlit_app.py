533e976ef3d7417886445239182f5c08eedc865e
livecnus9 <105122087+livecnus9@users.noreply.github.com>
2022-05-07 12:13:51 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,12 @@
 import streamlit
 streamlit.title('My New Healthy Diner')
+streamlit.Header('Breakfast Menu') 
+streamlit.text('IDLI')
+streamlit.Header('DOSA')
+streamlit.Header('UPMA')
+streamlit.Header('PONGAL')
+streamlit.Header('POORI')
+streamlit.Header('TEA')
+streamlit.Header('COFFEE')
+
+
